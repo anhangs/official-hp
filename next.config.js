7cefed4ab3f69d4c3eeac1f9903258e240611871
masaaa1011 <elle.hedgehog.diaries.26masa08@gmail.com>
2024-01-28 03:55:34 +0900

    💚 delete gql

--- next.config.js
+++ next.config.js
@@ -9,12 +9,6 @@ module.exports = {
   },
   webpack: (config, options) => {
     config.module.rules.push({
-      test: /\.(graphql|gql)/,
-      exclude: /node_modules/,
-      loader: 'graphql-tag/loader',
-    });
-
-    config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
       use: ['raw-loader', 'glslify-loader'],
